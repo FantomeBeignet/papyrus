17b1593eb5731eb8a28839000a92cfd351c8ee95
FantomeBeignet <himself@fantomebeig.net>
2023-03-01 20:02:40 +0100

    feat: scaffolding for init command

--- lib/cmd.ml
+++ lib/cmd.ml
@@ -8,6 +8,13 @@ let dev =
   Command.basic ~summary:"Run the dev server"
     (Command.Param.return (fun () -> print_endline "dev"))
 
+let init =
+  let open Command.Let_syntax in
+  Command.basic ~summary:"Initialize a Papyrus project"
+    [%map_open
+      let dir = anon ("dir" %: string) in
+      fun () -> Printf.printf "init %s" dir]
+
 let papyrus =
   Command.group ~summary:"Create a documentation site from markdown"
-    [ ("build", build); ("dev", dev) ]
+    [ ("build", build); ("dev", dev); ("init", init) ]
