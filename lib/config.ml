5ec93be3adc4fc694146a55a8122a17bc6b6bc44
FantomeBeignet <himself@fantomebeig.net>
2023-03-02 09:52:57 +0100

    feat: function to dump default config

--- lib/config.ml
+++ lib/config.ml
@@ -10,4 +10,10 @@ type t = {
 }
 [@@deriving sexp, show { with_path = false }]
 
-let load_config file = Sexp.load_sexp file |> t_of_sexp
+let load_config file = Sexp.load_sexp_conv_exn file t_of_sexp
+let default_config = Sexp.of_string "()" |> t_of_sexp
+
+let dump_default_config file =
+  let out = Out_channel.create file in
+  let str = sexp_of_t default_config |> Sexp_pretty.sexp_to_string in
+  Printf.fprintf out "%s" str
