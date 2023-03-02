82cb2206dcee8272fa70c80150e72791d64267ef
FantomeBeignet <himself@fantomebeig.net>
2023-03-02 08:29:26 +0100

    feat: base of config for projects

---
+++ lib/config.ml
@@ -0,0 +1,10 @@
+open Core
+
+type t = {
+  name : string option; [@sexp.option]
+  root_dir : string; [@default ""] [@sexp_drop_default String.equal]
+  routes : (string * string) list; [@sexp.list]
+}
+[@@deriving sexp, show]
+
+let load_config file = Sexp.load_sexp file |> t_of_sexp
