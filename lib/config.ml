c66bb9ccc3bef24d8979e7afb08e0e37c0d5fd0e
FantomeBeignet <himself@fantomebeig.net>
2023-03-02 09:52:38 +0100

    feat: more default config options

--- lib/config.ml
+++ lib/config.ml
@@ -1,11 +1,13 @@
 open Core
 
 type t = {
-  name : string option; [@sexp.option]
+  name : string; [@default "Papyrus"]
+  description : string; [@default "A sample Papyrus project"]
+  authors : string list; [@sexp.list]
+  language : string; [@default "en"]
   root_dir : string; [@default ""] [@sexp_drop_default String.equal]
   routes : (string * string) list; [@sexp.list]
 }
-[@@deriving sexp, show]
 [@@deriving sexp, show { with_path = false }]
 
 let load_config file = Sexp.load_sexp file |> t_of_sexp
