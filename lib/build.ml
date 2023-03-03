74985227586f090fb0b6629a2178e94d7bfaba46
FantomeBeignet <himself@fantomebeig.net>
2023-03-03 17:45:54 +0100

    feat: preserve dir structure on build

--- lib/build.ml
+++ lib/build.ml
@@ -10,28 +10,34 @@ let build_file from _to =
 let build_project (config : Config.t) =
   mkdir_p (Filename.concat config.build_dir config.root_dir);
   let content_dir = Filename.concat "src" "content" in
-  let build_dir = Filename.concat config.build_dir config.root_dir in
-  (match Sys_unix.is_directory build_dir with
+  let build_root = Filename.concat config.build_dir config.root_dir in
+  (match Sys_unix.is_directory build_root with
   | `Yes -> ()
-  | _ -> mkdir_p build_dir);
-  let rec aux = function
+  | _ -> mkdir_p build_root);
+  let rec aux dir files =
+    let build_dir_path = Filename.concat build_root dir in
+    (match Sys_unix.is_directory build_dir_path with
+    | `Yes -> ()
+    | _ -> mkdir_p build_dir_path);
+    match files with
     | [] -> ()
     | filename :: tl -> (
-        let filepath = Filename.concat content_dir filename in
-        match Sys_unix.is_directory filepath with
+        let filepath = Filename.concat dir filename in
+        let projected_file_path = Filename.concat build_root filepath in
+        let real_file_path = Filename.concat content_dir filepath in
+        match Sys_unix.is_directory real_file_path with
         | `Yes ->
-            aux (Sys_unix.ls_dir filepath);
-            aux tl
+            aux filepath (Sys_unix.ls_dir real_file_path);
+            aux dir tl
         | _ when Filename.check_suffix filename "md" ->
-            build_file filepath
-              (Filename.concat build_dir
-                 (Filename.chop_suffix filename "md" ^ "html"));
-            aux tl
+            build_file real_file_path
+              (Filename.chop_suffix projected_file_path "md" ^ "html");
+            aux dir tl
         | _ ->
-            Utils.cp filepath (Filename.concat build_dir filename);
-            aux tl)
+            Utils.cp real_file_path projected_file_path;
+            aux dir tl)
   in
-  aux (Sys_unix.ls_dir content_dir)
+  aux "" (Sys_unix.ls_dir content_dir)
 
 let build_command () =
   let config = Config.find_config () in
