515b2d4b4cb3d81f7f6ae2175bccb6a92f9c9711
FantomeBeignet <himself@fantomebeig.net>
2023-03-03 17:45:40 +0100

    fix chop_prefix

--- lib/utils.ml
+++ lib/utils.ml
@@ -6,3 +6,12 @@ let cp source dest =
   | _ ->
       let contents = In_channel.read_all source in
       Out_channel.write_all dest ~data:contents
+
+let chop_prefix ~prefix filename =
+  let len_p = String.length prefix and len_f = String.length filename in
+  if len_f >= len_p then
+    let pre = String.sub filename 0 len_p in
+    if pre = prefix then
+      Some (String.sub filename (len_p + 1) (len_f - len_p - 1))
+    else None
+  else None
