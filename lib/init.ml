253da1edd3281674b77d7b27d5efa2c89f138519
FantomeBeignet <himself@fantomebeig.net>
2023-03-06 18:21:49 +0100

    feat: added description choice on project init

--- lib/init.ml
+++ lib/init.ml
@@ -3,8 +3,8 @@ open Sys_unix
 open Defaults
 module C = Cmdliner
 
-let init_project title gitignore =
-  let config = Config.make_config ~title () in
+let init_project title description gitignore =
+  let config = Config.make_config ~title ~description () in
   mkdir_p (Filename.concat (Filename.concat title "src") "content");
   mkdir (Filename.concat title "_papyrus");
   Config.dump_config (Filename.concat title (title ^ ".papyrus")) config;
@@ -14,7 +14,7 @@ let init_project title gitignore =
   print_endline [%string "Papyrus project %{Filename.quote title} created"];
   Out_channel.flush Stdlib.stdout
 
-let init title gitignore =
+let init title description gitignore =
   Printf.printf "Welcome to Papyrus!\n";
   (match title with
   | None ->
@@ -54,19 +54,38 @@ let init title gitignore =
       | Some _ -> true)
     else true
   in
-  init_project project_name _gitignore
+  let _description =
+    match description with
+    | Some s -> s
+    | None -> (
+        print_endline "Project description:";
+        print_string "> ";
+        Out_channel.flush Stdlib.stdout;
+        let input = In_channel.input_line Stdlib.stdin in
+        match input with Some s -> s | None -> failwith "Exited")
+  in
+  init_project project_name _description _gitignore
 
 let title_term =
   let info = C.Arg.info [] ~doc:"The name of your project" ~docv:"NAME" in
   C.Arg.value (C.Arg.pos 0 (C.Arg.some C.Arg.string) None info)
 
+let description_term =
+  let info =
+    C.Arg.info [ "d"; "description" ] ~docv:"DESCRIPTION"
+      ~doc:"Description of the project"
+  in
+  C.Arg.value (C.Arg.opt (C.Arg.some C.Arg.string) None info)
+
 let gitignore_term =
   let info =
-    C.Arg.info [ "gitignore" ] ~doc:"Create a .gitignore in your project"
+    C.Arg.info [ "gitignore" ] ~docv:"GITIGNORE"
+      ~doc:"Create a .gitignore in your project"
   in
   C.Arg.value (C.Arg.flag info)
 
-let init_term = C.Term.(const init $ title_term $ gitignore_term)
+let init_term =
+  C.Term.(const init $ title_term $ description_term $ gitignore_term)
 
 let init_command =
   let doc = "Initialise a Papyrus project" in
