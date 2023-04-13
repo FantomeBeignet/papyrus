1f7aac24f867bd3b79b1d74277d50efcfb17051d
Tom Béné <himself@fantomebeig.net>
2023-04-13 22:09:46 +0200

    feat: path concat function and more core

--- lib/utils.ml
+++ lib/utils.ml
@@ -1,3 +1,4 @@
+open Core
 open Stdio
 
 let cp source dest =
@@ -10,9 +11,9 @@ let cp source dest =
 let chop_prefix ~prefix filename =
   let len_p = String.length prefix and len_f = String.length filename in
   if len_f >= len_p then
-    let pre = String.sub filename 0 len_p in
-    if pre = prefix then
-      Some (String.sub filename (len_p + 1) (len_f - len_p - 1))
+    let pre = String.sub filename ~pos:0 ~len:len_p in
+    if String.equal pre prefix then
+      Some (String.sub filename ~pos:(len_p + 1) ~len:(len_f - len_p - 1))
     else None
   else None
 
@@ -23,4 +24,7 @@ let get_fields_align fields =
     | _ :: tl -> aux acc tl
   in
   let max_field_length = aux 0 fields in
-  List.map (fun (elt, _) -> max_field_length - String.length elt) fields
+  List.map ~f:(fun (elt, _) -> max_field_length - String.length elt) fields
+
+let concat_paths p1 p2 = 
+  if String.is_empty p1 then p2 else Filename.concat p1 p2
