cb266e491b18a89ec7dcae02e5843da704dc8912
Tom Béné <himself@fantomebeig.net>
2023-04-13 22:37:57 +0200

    feat: write output file to correct location according to config

--- lib/build.ml
+++ lib/build.ml
@@ -1,3 +1,4 @@
+open Core
 open Core_unix
 module Ic = Stdio.In_channel
 module Oc = Stdio.Out_channel
@@ -6,34 +7,41 @@ module C = Cmdliner
 let build_file (config : Config.t) source dest =
   Ic.read_all source
   |> Html.render_page ~lang:config.language ~title:config.title
-    ~description:config.description ~authors:config.authors
+       ~description:config.description ~authors:config.authors
   |> fun c -> Oc.write_all dest ~data:c
 
 let build_project (config : Config.t) =
-  mkdir_p (Filename.concat config.build_dir config.base_url);
-  let content_dir = Filename.concat "src" "content" in
-  let build_root = Filename.concat config.build_dir config.base_url in
+  mkdir_p (Utils.concat_paths config.build_dir config.base_url);
+  let content_dir = Utils.concat_paths "src" "content" in
+  let build_root = Utils.concat_paths config.build_dir config.base_url in
+  let routes_map = Config.routes_to_map config.routes in
+  let map_filename path =
+    let dirname = Filename.dirname path and basename = Filename.basename path in
+    match Map.find routes_map basename with
+    | Some r -> Utils.concat_paths dirname r
+    | None -> Filename.chop_suffix path ".md"
+  in
   (match Sys_unix.is_directory build_root with
   | `Yes -> ()
   | _ -> mkdir_p build_root);
   let rec aux dir files =
-    let build_dir_path = Filename.concat build_root dir in
+    let build_dir_path = Utils.concat_paths build_root dir in
     (match Sys_unix.is_directory build_dir_path with
     | `Yes -> ()
     | _ -> mkdir_p build_dir_path);
     match files with
     | [] -> ()
     | filename :: tl -> (
-        let filepath = Filename.concat dir filename in
-        let projected_file_path = Filename.concat build_root filepath in
-        let real_file_path = Filename.concat content_dir filepath in
+        let filepath = Utils.concat_paths dir filename in
+        let projected_file_path = Utils.concat_paths build_root filepath in
+        let real_file_path = Utils.concat_paths content_dir filepath in
         match Sys_unix.is_directory real_file_path with
         | `Yes ->
             aux filepath (Sys_unix.ls_dir real_file_path);
             aux dir tl
         | _ when Filename.check_suffix filename "md" ->
             build_file config real_file_path
-              (Filename.chop_suffix projected_file_path "md" ^ "html");
+              (map_filename projected_file_path ^ ".html");
             aux dir tl
         | _ ->
             Utils.cp real_file_path projected_file_path;
